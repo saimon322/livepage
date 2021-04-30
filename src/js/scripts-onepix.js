b230d97dcfe4ac3cf8b9e5025e08a19315ac38c6
Siemens <saimon.kat23@gmail.com>
2021-04-30 13:23:37 +0300

    single case page update

--- src/js/scripts-onepix.js
+++ src/js/scripts-onepix.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
-    if ($('.new-page').length) {
-        // Tabs nav
-        $('.tabs__link').on('click', function (e) {
+    // Tabs nav
+    const $tabsLink = $('.tabs__link');
+    $tabsLink &&
+        $tabsLink.on('click', function (e) {
             e.preventDefault();
             let tab = $(this).attr('href'),
                 tabs = $(this).parents('.tabs'),
@@ -17,8 +18,10 @@ $(document).ready(function () {
             return false;
         })
 
-        // Positions levels
-        $('.position__level').on('click', function (e) {
+    // Positions levels
+    const $positionLevel = $('.position__level');
+    $positionLevel &&
+        $positionLevel.on('click', function (e) {
             e.preventDefault();
             let tab = $(this).attr('href'),
                 tabs = $('.levels');
@@ -29,8 +32,10 @@ $(document).ready(function () {
             return false;
         })
 
-        // Reviews carousel
-        $('.reviews-slider').owlCarousel({
+    // Reviews carousel
+    const $rewiesSlider = $('.reviews-slider');
+    $rewiesSlider &&
+        $rewiesSlider.owlCarousel({
             items : 1,
             autoHeight : true,
             dots : false,
@@ -49,108 +54,110 @@ $(document).ready(function () {
             nav.animate({scrollLeft: newScroll}, 400);
         });
 
-        // File upload processing
-        $upload = $('.file-upload');
-        if ($upload.length) {
-            let fileUploadFields = document.querySelectorAll('.file-upload__field'),
-                $form = $('.form'),
-                $input = ('.file-upload__field'),
-                $label = $('.file-upload__name');
+    // File upload processing
+    $upload = $('.file-upload');
+    if ($upload.length) {
+        let fileUploadFields = document.querySelectorAll('.file-upload__field'),
+            $form = $('.form'),
+            $input = ('.file-upload__field'),
+            $label = $('.file-upload__name');
 
-            function fileOnLoad(files) {
-                let fileName = files[0].name;
+        function fileOnLoad(files) {
+            let fileName = files[0].name;
 
-                if (fileName) {
-                    $label.text(fileName);
-                    $upload.addClass('file-upload--loaded');
-                } else {
-                    $upload.removeClass('file-upload--loaded');
-                }
+            if (fileName) {
+                $label.text(fileName);
+                $upload.addClass('file-upload--loaded');
+            } else {
+                $upload.removeClass('file-upload--loaded');
             }
+        }
 
-            Array.prototype.forEach.call(fileUploadFields, (input) => {
-                input.addEventListener('change', function () {
-                    fileOnLoad(this.files);
-                }, { passive: true });
-            });
+        Array.prototype.forEach.call(fileUploadFields, (input) => {
+            input.addEventListener('change', function () {
+                fileOnLoad(this.files);
+            }, { passive: true });
+        });
 
-            // Advanced upload (drag n drop)
-            var isAdvancedUpload = function () {
-                var div = document.createElement('div');
-                return (('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) && 'FormData' in window && 'FileReader' in window;
-            }();
+        // Advanced upload (drag n drop)
+        var isAdvancedUpload = function () {
+            var div = document.createElement('div');
+            return (('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) && 'FormData' in window && 'FileReader' in window;
+        }();
 
-            if (isAdvancedUpload) {
-                $upload.addClass('has-advanced-upload');
-                var droppedFiles = false;
+        if (isAdvancedUpload) {
+            $upload.addClass('has-advanced-upload');
+            var droppedFiles = false;
 
-                $upload.on('drag dragstart dragend dragover dragenter dragleave drop', function (e) {
-                    e.preventDefault();
-                    e.stopPropagation();
+            $upload.on('drag dragstart dragend dragover dragenter dragleave drop', function (e) {
+                e.preventDefault();
+                e.stopPropagation();
+            })
+                .on('dragover dragenter', function () {
+                    $upload.addClass('is-dragover');
                 })
-                    .on('dragover dragenter', function () {
-                        $upload.addClass('is-dragover');
-                    })
-                    .on('dragleave dragend drop', function () {
-                        $upload.removeClass('is-dragover');
-                    })
-                    .on('drop', function (e) {
-                        droppedFiles = e.originalEvent.dataTransfer.files;
-                        fileOnLoad(droppedFiles);
-                    });
+                .on('dragleave dragend drop', function () {
+                    $upload.removeClass('is-dragover');
+                })
+                .on('drop', function (e) {
+                    droppedFiles = e.originalEvent.dataTransfer.files;
+                    fileOnLoad(droppedFiles);
+                });
 
-                var ajaxData = new FormData($form.get(0));
+            var ajaxData = new FormData($form.get(0));
 
-                if (droppedFiles) {
-                    $.each(droppedFiles, function (i, file) {
-                        ajaxData.append($input.attr('name'), file);
-                    });
-                }
-
-                $.ajax({
-                    url: $form.attr('action'),
-                    type: $form.attr('method'),
-                    data: ajaxData,
-                    dataType: 'json',
-                    cache: false,
-                    contentType: false,
-                    processData: false,
-                    complete: function () {
-                        $form.removeClass('is-uploading');
-                    },
-                    success: function (data) {
-                        $form.addClass(data.success == true ? 'is-success' : 'is-error');
-                        if (!data.success) $errorMsg.text(data.error);
-                    },
-                    error: function () {
-                        console.log('Form submitting error');
-                    }
+            if (droppedFiles) {
+                $.each(droppedFiles, function (i, file) {
+                    ajaxData.append($input.attr('name'), file);
                 });
             }
-        }
 
-        // Single case carousel
-        const caseSliderQuery = '.single-case-slider';
-        if ($(caseSliderQuery).length) {
-            const caseSlider = new Swiper(caseSliderQuery, {
-                slidesPerView : 3,
-                slideToClickedSlide: true,
-                centeredSlides: true,
-                loop: true,
-                speed: 600,
-                autoplay: {
-                    delay: 2000,
-                    disableOnInteraction: false
+            $.ajax({
+                url: $form.attr('action'),
+                type: $form.attr('method'),
+                data: ajaxData,
+                dataType: 'json',
+                cache: false,
+                contentType: false,
+                processData: false,
+                complete: function () {
+                    $form.removeClass('is-uploading');
                 },
-            })
+                success: function (data) {
+                    $form.addClass(data.success == true ? 'is-success' : 'is-error');
+                    if (!data.success) $errorMsg.text(data.error);
+                },
+                error: function () {
+                    console.log('Form submitting error');
+                }
+            });
         }
+    }
 
-        // vh fix
+    // Single case AOS animations
+    
+
+    // Single case carousel
+    const caseSliderQuery = '.single-case-slider';
+    if ($(caseSliderQuery).length) {
+        const caseSlider = new Swiper(caseSliderQuery, {
+            slidesPerView : 3,
+            slideToClickedSlide: true,
+            centeredSlides: true,
+            loop: true,
+            speed: 600,
+            autoplay: {
+                delay: 2000,
+                disableOnInteraction: false
+            },
+        })
+    }
+
+    // vh fix
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', vh + 'px');
+    window.addEventListener('resize', () => {
         let vh = window.innerHeight * 0.01;
         document.documentElement.style.setProperty('--vh', vh + 'px');
-        window.addEventListener('resize', () => {
-            let vh = window.innerHeight * 0.01;
-            document.documentElement.style.setProperty('--vh', vh + 'px');
-        }, {passive: true});
-    }
+    }, {passive: true});
 })
